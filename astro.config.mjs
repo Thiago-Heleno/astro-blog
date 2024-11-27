3f3dcbfdd954b12eb4cb649fc3e33a2691c328d5
Thiago-Heleno <tibis0313@gmail.com>
2024-11-26 21:40:53 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({ applyBaseStyles: false, })],
-  site: 'https://Thiago-Heleno.github.io',
-  base: '/astro-landing-page/',
   output: 'hybrid',
   adapter: vercel({
     webAnalytics: {
